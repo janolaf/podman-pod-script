2313e854bae7ab34faabad0d824b33bb0c824553
Jan-Olaf Bakker <janolaf@gmail.com>
2022-10-31 22:24:33 -0500

    fixed conflict

--- mealie/mealie.sh
+++ mealie/mealie.sh
@@ -13,17 +13,3 @@ podman container create --name mealie-frontend --pod mealie \
 	-v /opt/mealie/data:/app/data:z \
 	-e API_URL=http://mealie-api:9000 \
 	hkotel/mealie:frontend-v1.0.0beta-5
-=======
-        -v /opt/mealie/data:/app/data:z \
-        -e ALLOW_SIGNUP=true \
-        -e MAX_WORKERS=1 \
-        -e WEB_CONCURRENCY=1 \
-        -e TZ=UTC \
-        -e BASE_URL=http://food.home.janolaf.ca \
-        hkotel/mealie:api-v1.0.0beta-4
-
-podman container create --name mealie-frontend --pod mealie \
-        -v /opt/mealie/data:/app/data:z \
-        -e API_URL=http://mealie-api:9000 \
-        hkotel/mealie:frontend-v1.0.0beta-4
->>>>>>> 87c5c6ab1a31c10cb08001edf2f070aa50d0b801
