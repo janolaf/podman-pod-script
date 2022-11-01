d217e6be3a09ed4545b84504de589d0dc0c608ba
Jan-Olaf Bakker <janolaf@gmail.com>
2022-10-31 22:20:44 -0500

    update to beta5

--- mealie/mealie.sh
+++ mealie/mealie.sh
@@ -5,11 +5,10 @@ podman container create --name mealie-api --pod mealie \
 	-e ALLOW_SIGNUP=true \
 	-e MAX_WORKERS=1 \
 	-e WEB_CONCURRENCY=1 \
-	-e TZ=UTC \
        	-e BASE_URL=http://food.home.janolaf.ca \
-	hkotel/mealie:api-v1.0.0beta-4
+	hkotel/mealie:api-v1.0.0beta-5
 
 podman container create --name mealie-frontend --pod mealie \
 	-v /opt/mealie/data:/app/data:z \
 	-e API_URL=http://mealie-api:9000 \
-	hkotel/mealie:frontend-v1.0.0beta-4
+	hkotel/mealie:frontend-v1.0.0beta-5
