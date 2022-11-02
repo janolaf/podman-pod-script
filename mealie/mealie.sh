681848116adaf689da57b827df25f849ca1819f4
Jan-Olaf Bakker <janolaf@gmail.com>
2022-11-02 18:18:40 -0500

    fixed

--- mealie/mealie.sh
+++ mealie/mealie.sh
@@ -1,7 +1,6 @@
-podman pod create --name mealie -p 10006:3000
+podman pod create --replace --name mealie -p 10006:3000
 
-podman container create --name mealie-api --pod mealie \
-<<<<<<< HEAD
+podman container create --replace --name mealie-api --pod mealie \
 	-v /opt/mealie/data:/app/data:z \
 	-e ALLOW_SIGNUP=true \
 	-e MAX_WORKERS=1 \
@@ -9,7 +8,7 @@ podman container create --name mealie-api --pod mealie \
        	-e BASE_URL=http://food.home.janolaf.ca \
 	hkotel/mealie:api-v1.0.0beta-5
 
-podman container create --name mealie-frontend --pod mealie \
+podman container create --replace --name mealie-frontend --pod mealie \
 	-v /opt/mealie/data:/app/data:z \
 	-e API_URL=http://mealie-api:9000 \
 	hkotel/mealie:frontend-v1.0.0beta-5
