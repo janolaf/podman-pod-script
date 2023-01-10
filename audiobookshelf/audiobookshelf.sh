af0dafb68ba8eb08ea03d1d13b5e8e9fb3169dea
Jan-Olaf Bakker <janolaf@gmail.com>
2023-01-10 10:03:22 -0600

    adding audiobookshelf

---
+++ audiobookshelf/audiobookshelf.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+podman pod create --name audiobookshelf-pod -p 10003:80 --replace
+
+# Must install udici to create SELinux policy. 
+
+podman container create --pod audiobookshelf-pod --name audiobookshelf-app \
+	--label io.containers.autoupdate=local \
+	--security-opt "label=type:audiobookshelf-app.process" \
+	-v /opt/audiobookshelf/config:/config:Z \
+	-v /opt/audiobookshelf/metadata:/metadata:Z \
+	-v /srv/share/music/audiobooks:/audiobooks:ro \
+	--replace \
+	ghcr.io/advplyr/audiobookshelf:latest
+
+# TODO -- Addd Udici instructions 
