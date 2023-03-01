926dab501a9bf9fd579824fbbc4c437fa416c57f
Jan-Olaf Bakker <janolaf@gmail.com>
2023-03-01 15:58:42 -0600

    add security context

--- audiobookshelf/audiobookshelf.sh
+++ audiobookshelf/audiobookshelf.sh
@@ -1,15 +1,15 @@
 #/bin/bash
-podman pod create --name audiobookshelf-pod -p 10003:80 --replace
+podman pod create --name audiobookshelf -p 10003:80 --replace
 
 # Must install udici to create SELinux policy. 
 
-podman container create --pod audiobookshelf-pod --name audiobookshelf-app \
+podman container create --pod audiobookshelf --name abs-app \
+	--replace \
 	--label io.containers.autoupdate=local \
-	--security-opt "label=type:audiobookshelf-app.process" \
+	--security-opt "label=type:audiobookshelf.process" \
 	-v /opt/audiobookshelf/config:/config:Z \
 	-v /opt/audiobookshelf/metadata:/metadata:Z \
-	-v /audiobooks:/audiobooks:ro \
-	--replace \
+	-v /path/to/audiobooks:/audiobooks:ro \
 	ghcr.io/advplyr/audiobookshelf:latest
 
 # TODO -- Addd Udici instructions 
