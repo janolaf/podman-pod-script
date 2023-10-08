c6afcdc895bf8bffece3f7f1cb3182932102cff7
janolaf <janolaf@gmail.com>
2023-10-08 14:05:15 -0500

    mealie-api and mealie-frontent are deprecated

---
+++ mealie/mealie-nightly-old.sh
@@ -0,0 +1,13 @@
+podman pod create --replace \
+	--name mealie -p 10006:9000
+
+podman container create --name mealie-app --pod mealie \
+	--replace \
+	--volume /opt/mealie/data:/app/data:z \
+	--tz=America/Winnipeg \
+	-e ALLOW_SIGNUP=true \
+	-e BASE_URL='http://food.home.janolaf.ca' \
+    -e MAX_WORKERS=1 \
+    -e WEB_CONCURRENCY=1 \
+	--label io.containers.autoupdate=registry ghcr.io/mealie-recipes/mealie:nightly \
+    ghcr.io/mealie-recipes/mealie:nightly
